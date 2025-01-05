744d51fbe03f616fff0adf477f805eb6be55c773
MeadShalehin <md.meadshalehin@gmail.com>
2025-01-05 17:34:45 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/swift_swap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.swift_swap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Center(child: Text("My Tickets")),
+      ),
+      body: Center(
+        child: Text("Ticket Info"),
+
+      ),
+    );
+  }
+}
